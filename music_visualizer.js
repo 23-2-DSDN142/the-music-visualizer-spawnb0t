c441b3729a3415ad231ed5c404816c8af0bc64a7
spawnb0t <139717697+spawnb0t@users.noreply.github.com>
2023-09-29 16:32:44 +1300

    Playing with Loops

--- music_visualizer.js
+++ music_visualizer.js
@@ -1,35 +1,84 @@
-//1280x720
+//1080x720
 
 //system_runner; change canvas size // vert or hor
 //system_settings; debugFastRefresh to have countdown delay record 
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(255)
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+  noFill()
 
-  let yellow = color(240, 197, 5)
-  let red = color(204, 14, 14)
-  let LerpMap = map(bass, 0, 100, 0,1)
-  let middleColor = lerpColor(yellow, red, LerpMap)
-  
-  let mappedXPost = map(vocal, 0, 100, -100, 100)
-  
-  fill(middleColor)
-  rect(width/2, height/2+height/4, 1280, 360)
-  ellipse(width/2, height/2, 100, 100)
+  // //gradient background & ellipse
+  // let purple = color(127, 37, 213)
+  // let pink = color(223, 73, 133)  
+  // let midPPLerp = map(vocal, 0, 100, 0, 1)
+  // let midPinkPurple = lerpColor(pink, purple, 0.85) //make 0.85 midPPLerp so it visualises vocals
 
+  // strokeWeight(3)
+  // let backgroundGradient = 1000
+
+  // for(let i=0; i<backgroundGradient; i++){
+  //   let gradientAmount = map(i, 0, backgroundGradient, 0,1)
+  //   let strokeColor = lerpColor(midPinkPurple, pink, gradientAmount)
+
+  //   stroke(strokeColor)
+  //   ellipse(width/2, height/2, i/4)
+  //   line(0, 100+i-100, width, 100+i-100)
+  // }
+
+
+  // // horizon colour change with bass
+  // let yellow = color(240, 197, 5)
+  // let red = color(204, 14, 14)
+  // let lerpMap = map(bass, 0, 100, 0,1)
+  // let middleColor = lerpColor(pink, purple, lerpMap)
+  
+  // let mappedXPost = map(vocal, 0, 100, -100, 100)
+  
+  // fill(middleColor)
+  // rect(width/2, height/2+height/4, 1280, 360)
+  // ellipse(width/2, height/2, 100, 100)
  
   //display "words"
-textAlign(CENTER);
-textSize(vocal);
-text(words, width/2, height/3);
+// text(0)
+// textAlign(CENTER);
+// textSize(other);
+// text(words, width/2, height/3);
    
 
+//eg effect - lines and colour changing with drum
+colorMode(HSB, 100)
+strokeWeight(7)
+stroke(drum, 80, 80)
+
+let drumMap = map(drum, 0, 100, 0, 90)
+let lineLength = 1080
+let lineStart = 0
+let lineEnd = lineStart+lineLength
+
+
+
+for(let i=1; i<drumMap; i++){
+let lineStep = i*8 - 5;
+line(lineStart, lineStep, lineEnd, lineStep)
 }
 
+//Wave visualiser
+stroke(0)
+strokeWeight(5)
+let waveHeight = map(drum, 0, 100, 20, 120);
+let drumWave;
+let waveFreq = 3;
+
+beginShape()
+for(let i = 0; i < width; i++){
+  vertex(i, height/2 - waveHeight*sin(waveFreq*i))
+}
+endShape()
+}
 
 
 
