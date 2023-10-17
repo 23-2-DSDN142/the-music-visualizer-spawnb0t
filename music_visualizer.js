3717947311356418e8df64d906a389bff71ccd54
spawnb0t <139717697+spawnb0t@users.noreply.github.com>
2023-10-18 11:07:19 +1300

    background timings

--- music_visualizer.js
+++ music_visualizer.js
@@ -25,7 +25,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   
     firstRun = false
   }
-  console.log(counter);
   console.log(song.duration())
   //let slowIncr = map(song.currentTime(), 0, song.duration(), 0, 2300);
 
@@ -54,13 +53,150 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
 
 
-
-
 //
 //
 //if statement controlling what is shown in each part of the song chronologically according to counter variable
-//intro - verse 1
-if(counter <= 135){
+
+//daytime city
+if((counter <= 140) || (counter >=255 && counter <=383) || (counter >=501 && counter <=1339) || (counter >=2060 && counter <=2799)){
+  //gradient background
+  let midPPLerp = map(vocal, 0, 100, 0, 1)
+  let midBlueYellow = lerpColor(blue, yellow, 1) //make 0.85 midPPLerp so it visualises vocals
+
+  strokeWeight(3)
+  let backgroundGradient = 1000
+
+  for(let i=0; i<backgroundGradient; i++){
+    let gradientAmount = map(i, 0, backgroundGradient, 0, 1.5)
+    let strokeColor = lerpColor(blue, yellow, gradientAmount)
+
+    stroke(strokeColor)
+    line(0, 100+i-100, width, 100+i-100)
+  }
+
+
+
+  //retrolines and colour changing with drum
+push()
+
+let drumMap = map(drum, 0, 100, 0, 90)
+let lineLength = 1080
+let lineStart = 0
+let lineEnd = lineStart+lineLength
+
+strokeWeight(7)
+let DrumColorMap = map(drum, 0, 100, 0,1) /// all one color, color change based on Drum volume
+
+for(let i=1; i<drumMap; i++){
+let lineStep = i*8 - 5;
+let gradientAmount = map(i, 0, drumMap, 0,1) // gradient of lines. colour change based on how far down the page the lines are
+let LinesStrokeColor = lerpColor(blue, yellow, gradientAmount) 
+stroke(LinesStrokeColor)
+line(lineStart, lineStep, lineEnd, lineStep)
+}
+pop()
+
+
+
+//draw building layer behind sun
+buildingsBehindImg.resize(1080, 220)
+image(buildingsBehindImg, 0, 80);
+
+
+//sun
+for(let i=0; i<backgroundGradient; i++){
+  let gradientAmount = map(i, 0, backgroundGradient, 0,1)
+  let SunStrokeColor = lerpColor(red, yellow, gradientAmount)
+
+  stroke(SunStrokeColor)
+  ellipse(width/2, height/4+75, i/4)
+}
+
+
+
+//draw horizon 
+stroke(0);
+fill(180);
+beginShape();
+vertex(0, height);
+vertex(0, height/2-90);
+vertex(width/2-40, height/2-65);
+vertex(width/2+40, height/2-65);
+vertex(width, height/2-90);
+vertex(width, height);
+endShape();
+
+
+
+//draw road
+stroke(0);
+fill(100);
+beginShape();
+vertex(width/2-40, height/2-65);
+vertex(width/2+40, height/2-65);
+vertex(width, height/2+50);
+vertex(width, height);
+vertex(0, height);
+vertex(0, height/2+210)
+vertex(width/2-40, height/2-65);
+endShape()
+ 
+stroke(255);
+strokeWeight(2);
+line(width/2+500, height, width/2, height/2-65)
+
+
+
+// //draw building layers
+// buildingsFrontImg.resize(1080, 300)
+// image(buildingsFrontImg, 0, 80);
+
+
+// //trailling being inside city
+// cityImg.resize(500, 550)
+// image(cityImg, 0, 20);
+
+ // display "words"
+ stroke(255);
+ textAlign(CENTER);
+ textSize(other);
+ text(words, width/2, height/3);  
+
+
+
+ //car frame
+carFrameImg.resize(1080, 720)
+image(carFrameImg, 0, 0);
+
+
+//Wave visualiser
+push();
+translate(600, 400);
+scale(0.3);
+
+stroke(255)
+strokeWeight(5)
+let waveHeight = map(other, 0, 50, 10, 60);
+let drumWave;
+let waveFreq = 3;
+
+beginShape();
+for(let i = 0; i < width/2.2; i++){
+  vertex(i, height/2 - waveHeight*sin(waveFreq*i))
+}
+endShape();
+
+pop();
+
+}
+
+
+
+
+//
+//
+//retro city
+if((counter >= 141 && counter <=254) || (counter >=384 && counter <=500) || (counter >= 1340 && counter <= 2059)|| (counter >=2800 && counter <=4459)){
   //gradient background
   let midPPLerp = map(vocal, 0, 100, 0, 1)
   let midPinkPurple = lerpColor(pink, purple, 1) //make 0.85 midPPLerp so it visualises vocals
@@ -155,8 +291,8 @@ line(width/2+500, height, width/2, height/2-65)
 
 
 //trailling being inside city
-cityImg.resize(500, 550)
-image(cityImg, 0, 20);
+// cityImg.resize(500, 550)
+// image(cityImg, 0, 20);
 
  // display "words"
  stroke(255);
@@ -195,16 +331,11 @@ pop();
 
 
 
-
-
-
-
-
 //
 //
 //
-//chorus first time
-else if(counter > 135 && counter <=380){
+//dark moonscape
+else if(counter >=4460){
   //gradient background
   let midPPLerp = map(vocal, 0, 100, 0, 1)
   let midPinkPurple = lerpColor(pink, purple, 1) //make 0.85 midPPLerp so it visualises vocals
@@ -327,10 +458,15 @@ pop();
 
 
 
+
+
+
+
+
 //
 //
 //
-else{
+else if(counter >8000){
   
 background(0)
 
@@ -338,9 +474,7 @@ background(0)
 image(heartImg, 420, 100);
 
 }
-
 }
-
 
 
 
