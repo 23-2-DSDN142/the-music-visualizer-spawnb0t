fad2668a58d3f45167f7d41bfb7cf49f9bb78122
spawnb0t <139717697+spawnb0t@users.noreply.github.com>
2023-10-19 03:58:39 +1300

    Updates Thursday 19 Oct Morning

--- music_visualizer.js
+++ music_visualizer.js
@@ -10,7 +10,9 @@ let carFrameImg;
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   if(firstRun){
     rectMode(CENTER);
-    carFrameImg = loadImage('assets/CarFrame.png');
+    carFrame2Img = loadImage('assets/carFrame2.png');
+    needleImg = loadImage('assets/needle.png');
+    wheelImg = loadImage('assets/wheel.png');
     buildingsFrontImg = loadImage('assets/buildingsFront.png');
     buildingsBehindImg = loadImage('assets/buildingsBehind.png');
     moonImg = loadImage('assets/moon.png');
@@ -21,7 +23,16 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     mountainsFrontImg = loadImage('assets/mountainsFront.png');
     mountainsBehindImg = loadImage('assets/mountainsBehind.png');
     cityImg = loadImage('assets/city.png');
-    
+    sunLImg = loadImage('assets/sunL.png');
+    sunRImg = loadImage('assets/sunR.png');
+    roadLineImg = loadImage('assets/roadLine.png');
+
+
+    lineStartX = (width/2);
+    lineStartY = (height/2-65);
+    lineOffsetX = (4.7*1.2);
+    lineOffsetY = (4*1.2); 
+  
   
     firstRun = false
   }
@@ -47,10 +58,9 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   let grey = color(120)
   let SunStrokeColor = lerpColor(pink, purple, 0)
 
-
-
-
-
+  strokeWeight(3)
+  let backgroundGradient = 1000
+  
 
 
 //
@@ -58,13 +68,10 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 //if statement controlling what is shown in each part of the song chronologically according to counter variable
 
 //daytime city
-if((counter <= 140) || (counter >=255 && counter <=383) || (counter >=501 && counter <=1339) || (counter >=2060 && counter <=2799)){
+if((counter <= 135) || (counter >=250 && counter <=1339) || (counter >=2060 && counter <=2799)){
   //gradient background
   let midPPLerp = map(vocal, 0, 100, 0, 1)
   let midBlueYellow = lerpColor(blue, yellow, 1) //make 0.85 midPPLerp so it visualises vocals
-
-  strokeWeight(3)
-  let backgroundGradient = 1000
 
   for(let i=0; i<backgroundGradient; i++){
     let gradientAmount = map(i, 0, backgroundGradient, 0, 1.5)
@@ -73,7 +80,6 @@ if((counter <= 140) || (counter >=255 && counter <=383) || (counter >=501 && cou
     stroke(strokeColor)
     line(0, 100+i-100, width, 100+i-100)
   }
-
 
 
   //retrolines and colour changing with drum
@@ -103,14 +109,11 @@ buildingsBehindImg.resize(1080, 220)
 image(buildingsBehindImg, 0, 80);
 
 
-//sun
-for(let i=0; i<backgroundGradient; i++){
-  let gradientAmount = map(i, 0, backgroundGradient, 0,1)
-  let SunStrokeColor = lerpColor(red, yellow, gradientAmount)
 
-  stroke(SunStrokeColor)
-  ellipse(width/2, height/4+75, i/4)
-}
+
+
+
+
 
 
 
@@ -143,7 +146,10 @@ endShape()
  
 stroke(255);
 strokeWeight(2);
-line(width/2+500, height, width/2, height/2-65)
+// line(width/2+500, height, width/2, height/2-65)
+
+
+
 
 
 
@@ -156,47 +162,14 @@ line(width/2+500, height, width/2, height/2-65)
 // cityImg.resize(500, 550)
 // image(cityImg, 0, 20);
 
- // display "words"
- stroke(255);
- textAlign(CENTER);
- textSize(other);
- text(words, width/2, height/3);  
-
-
-
- //car frame
-carFrameImg.resize(1080, 720)
-image(carFrameImg, 0, 0);
-
-
-//Wave visualiser
-push();
-translate(600, 400);
-scale(0.3);
-
-stroke(255)
-strokeWeight(5)
-let waveHeight = map(other, 0, 50, 10, 60);
-let drumWave;
-let waveFreq = 3;
-
-beginShape();
-for(let i = 0; i < width/2.2; i++){
-  vertex(i, height/2 - waveHeight*sin(waveFreq*i))
-}
-endShape();
-
-pop();
 
 }
-
-
-
 
 //
 //
 //retro city
-if((counter >= 141 && counter <=254) || (counter >=384 && counter <=500) || (counter >= 1340 && counter <= 2059)|| (counter >=2800 && counter <=4459)){
+if((counter >= 136 && counter <=249) || 
+(counter >= 1340 && counter <= 2059)|| (counter >=2800 && counter <=4459)){
   //gradient background
   let midPPLerp = map(vocal, 0, 100, 0, 1)
   let midPinkPurple = lerpColor(pink, purple, 1) //make 0.85 midPPLerp so it visualises vocals
@@ -241,14 +214,7 @@ buildingsBehindImg.resize(1080, 220)
 image(buildingsBehindImg, 0, 80);
 
 
-//sun
-for(let i=0; i<backgroundGradient; i++){
-  let gradientAmount = map(i, 0, backgroundGradient, 0,1)
-  let SunStrokeColor = lerpColor(red, yellow, gradientAmount)
 
-  stroke(SunStrokeColor)
-  ellipse(width/2, height/4+75, i/4)
-}
 
 
 
@@ -278,10 +244,6 @@ vertex(0, height);
 vertex(0, height/2+210)
 vertex(width/2-40, height/2-65);
 endShape()
- 
-stroke(255);
-strokeWeight(2);
-line(width/2+500, height, width/2, height/2-65)
 
 
 
@@ -294,42 +256,7 @@ line(width/2+500, height, width/2, height/2-65)
 // cityImg.resize(500, 550)
 // image(cityImg, 0, 20);
 
- // display "words"
- stroke(255);
- textAlign(CENTER);
- textSize(other);
- text(words, width/2, height/3);  
-
-
-
- //car frame
-carFrameImg.resize(1080, 720)
-image(carFrameImg, 0, 0);
-
-
-//Wave visualiser
-push();
-translate(600, 400);
-scale(0.3);
-
-stroke(255)
-strokeWeight(5)
-let waveHeight = map(other, 0, 50, 10, 60);
-let drumWave;
-let waveFreq = 3;
-
-beginShape();
-for(let i = 0; i < width/2.2; i++){
-  vertex(i, height/2 - waveHeight*sin(waveFreq*i))
 }
-endShape();
-
-pop();
-
-}
-
-
-
 
 //
 //
@@ -384,6 +311,7 @@ moonImg.resize(250, 250)
 image(moonImg, width/2-125, 130);
 
 
+
 //draw horizon 
 stroke(0);
 fill(180);
@@ -413,46 +341,12 @@ endShape()
  
 stroke(255);
 strokeWeight(2);
-line(width/2+500, height, width/2, height/2-65)
-
-
-
-
-
-
- // display "words"
- stroke(255);
- textAlign(CENTER);
- textSize(other);
- text(words, width/2, height/3);  
+line(width/2+500, height, width/2, height/2-65) 
 
 //mountains
 mountainsFrontImg.resize(1080, 350)
 image(mountainsFrontImg, 0, 12);
 
- //car frame
-carFrameImg.resize(1080, 720)
-image(carFrameImg, 0, 0);
-
-
-//Wave visualiser
-push();
-translate(600, 400);
-scale(0.3);
-
-stroke(255)
-strokeWeight(5)
-let waveHeight = map(other, 0, 50, 10, 60);
-let drumWave;
-let waveFreq = 3;
-
-beginShape();
-for(let i = 0; i < width/2.2; i++){
-  vertex(i, height/2 - waveHeight*sin(waveFreq*i))
-}
-endShape();
-
-pop();
 
 }
 
@@ -474,6 +368,122 @@ background(0)
 image(heartImg, 420, 100);
 
 }
+
+
+
+
+
+
+heartScale = map(drum, 0, 100, 1, 1.2);
+//heart
+if ((counter >= 1250 && counter <= 2070)){
+  
+  push()
+  heartBackImg.resize(220*heartScale, 270*heartScale)
+  heartMainImg.resize(220*heartScale, 270*heartScale)
+  pop()
+
+  image(heartBackImg, 440, 100)
+  image(heartMainImg, 440, 100)
+
+  }
+  
+  //sun
+ 
+  if ((counter >= 1200 && counter <= 1320)){
+
+    push()
+    sunLImg.resize(282, 210)
+    image(sunLImg, 405, 115);
+    pop()
+
+    sunRImg.resize(282,210)
+    image(sunRImg, 405, 115);
+  
+  }
+  else if((counter >= 1351 )){
+  }
+  else{
+  for(let i=0; i<backgroundGradient; i++){
+    let gradientAmount = map(i, 0, backgroundGradient, 0,1)
+    let SunStrokeColor = lerpColor(red, yellow, gradientAmount)
+  
+    stroke(SunStrokeColor)
+    ellipse(width/2, height/4+75, i/4)
+  }
+  }
+
+
+
+
+
+
+
+
+//
+//
+//drawing constant car elements 
+
+// display "words"
+stroke(255);
+textAlign(CENTER);
+textSize(other);
+text(words, width/2, height/3);  
+
+
+//road line
+push()
+line(lineStartX, lineStartY, lineStartX+lineOffsetX, lineStartY+lineOffsetY);
+
+lineStartX = lineStartX + lineOffsetX
+lineStartY = lineStartY + lineOffsetY
+
+if(lineStartX > width/2+500){
+lineStartX = width/2
+}
+
+if(lineStartY > height){
+  lineStartY = height/2-65
+  }
+  pop()
+
+
+//car frame
+carFrame2Img.resize(1122, 820)
+image(carFrame2Img, -22, -10);
+
+//speedometer needle
+push()
+translate(375, 565)
+rotate(drum*3)
+needleImg.resize(25, 80)
+image(needleImg, 0, 0);
+pop()
+
+//wheel
+wheelImg.resize(500, 500)
+image(wheelImg, 125, 375);
+
+//Wave visualiser
+push();
+translate(685, 500);
+scale(0.22);
+
+noFill();
+stroke(255)
+strokeWeight(5)
+let waveHeight = map(vocal, 0, 50, 10, 60);
+let drumWave;
+let waveFreq = 3;
+
+beginShape();
+for(let i = 0; i < width/2.2; i++){
+ vertex(i, height/2 - waveHeight*sin(waveFreq*i))
+}
+endShape();
+
+pop();
+
 }
 
 
